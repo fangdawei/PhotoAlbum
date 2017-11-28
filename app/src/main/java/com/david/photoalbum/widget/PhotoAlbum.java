4e3d7fa3a2827b86e3c4b0a525357d372e702f19
fangdawei <fangdawei_www@163.com>
2017-11-28 20:53:52 +0800

    update

--- app/src/main/java/com/david/photoalbum/widget/PhotoAlbum.java
+++ app/src/main/java/com/david/photoalbum/widget/PhotoAlbum.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import com.bumptech.glide.Glide;
